a821e1fa259bbcf492c1ed097607cfb9192fbfae
magiczero <younghappy7@msn.com>
2015-07-31 14:11:41 +0800

    修改项目结构，功能模块分清

--- src/main/java/com/cngc/pm/model/Person.java
+++ src/main/java/com/cngc/pm/model/Person.java
@@ -7,6 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 /**
  * Entity bean with JPA annotations
  * Hibernate provides JPA implementation
@@ -14,23 +17,24 @@ import javax.persistence.Table;
  *
  */
 @Entity
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY) 
 @Table(name="PERSON")
 public class Person {
 
 	@Id
 	@Column(name="id")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 	
 	private String name;
 	
 	private String country;
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
--- src/main/java/com/cngc/pm/service/PersonServiceImpl.java
+++ src/main/java/com/cngc/pm/service/PersonServiceImpl.java
@@ -2,6 +2,7 @@ package com.cngc.pm.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,41 +11,41 @@ import com.cngc.pm.model.Person;
 
 @Service
 public class PersonServiceImpl implements PersonService {
-	
+	@Autowired
 	private PersonDAO personDAO;
-
+/*
 	public void setPersonDAO(PersonDAO personDAO) {
 		this.personDAO = personDAO;
 	}
-
+*/
 	@Override
 	@Transactional
 	public void addPerson(Person p) {
-		this.personDAO.addPerson(p);
+		this.personDAO.save(p);
 	}
 
 	@Override
 	@Transactional
 	public void updatePerson(Person p) {
-		this.personDAO.updatePerson(p);
+		this.personDAO.save(p);
 	}
 
 	@Override
 	@Transactional
 	public List<Person> listPersons() {
-		return this.personDAO.listPersons();
+		return this.personDAO.findAll();
 	}
 
 	@Override
 	@Transactional
 	public Person getPersonById(int id) {
-		return this.personDAO.getPersonById(id);
+		return this.personDAO.find(id);
 	}
 
 	@Override
 	@Transactional
 	public void removePerson(int id) {
-		this.personDAO.removePerson(id);
+		this.personDAO.removeById(id);
 	}
 
 }
